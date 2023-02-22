384b3c80478747740c267143b3831f1a7cc3b36d
Justin Kim <mletemps@hotmail.com>
2023-02-22 15:41:48 -0500

    used 'Props' naming convention

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Sidebar = ({ isCartOpen, setIsCartOpen }) => {
+const Sidebar = (props) => {
     return (
         <>
             <main
                 className={
                     " z-40 fixed overflow-hidden z-10 bg-gray-900 bg-opacity-50 inset-0 transform ease-in-out " +
-                    (isCartOpen
+                    (props.isCartOpen
                         ? " transition-opacity opacity-100 duration-300 translate-x-0  "
                         : " transition-all delay-300 opacity-0 translate-x-full  ")
                 }
@@ -14,7 +14,7 @@ const Sidebar = ({ isCartOpen, setIsCartOpen }) => {
                 <section
                     className={
                         " w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl  duration-300 ease-in-out transition-all transform  " +
-                        (isCartOpen ? " translate-x-0 " : " translate-x-full ")
+                        (props.isCartOpen ? " translate-x-0 " : " translate-x-full ")
                     }
                 >
                     <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
@@ -25,7 +25,7 @@ const Sidebar = ({ isCartOpen, setIsCartOpen }) => {
                 <section
                     className=" w-screen h-full cursor-pointer "
                     onClick={() => {
-                        setIsCartOpen(false);
+                        props.setIsCartOpen(false);
                     }}
                 ></section>
             </main>
