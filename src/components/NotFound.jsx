19ba08b39cfe9750f2b0ed09c9c1f11dc0222d00
Justin Kim <mletemps@hotmail.com>
2023-03-05 19:04:16 -0500

    environment variables now work with this react project

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 
 const NotFound = () => {
-    var test = "nice"
     return (
         <>
             <ul className="z-10 h-screen w-screen flex justify-center items-center place-content-around flex-col">
-                <li>{process.env.REACT_APP_TEST_VARIABLE}</li>
-                <li>{test}</li>
                 <li className="">404 page not found</li>
             </ul>
         </>
