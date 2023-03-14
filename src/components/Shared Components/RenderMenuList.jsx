b78d5feec41dc964fe6e584f0e04358837f105b1
Justin Kim <mletemps@hotmail.com>
2023-03-13 22:08:21 -0400

    updated styling to menu

--- src/components/Shared Components/RenderMenuList.jsx
+++ src/components/Shared Components/RenderMenuList.jsx
@@ -9,32 +9,37 @@ import {
     emptyCart,
 } from "../../state/cartSlice";
 
+import "./RenderMenuList.css";
+
 export function RenderList(list) {
     // define cart and dispatch for redux
     const cart = useSelector((state) => state.cart);
     const dispatch = useDispatch();
 
     const listItems = list.map((element) => (
-        <li key={uuid()}>
-            <div className="lg:max-w-2xl mb-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-stone-900 dark:border-stone-900 mx-5">
-                <a className="h-64">
+        <li
+            key={uuid()}
+            className="menuItem"
+        >
+            <div className=" lg:max-w-md mb-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-stone-900 dark:border-stone-900 mx-5">
+                <div className="  ">
                     <img
-                        className=" rounded-t-lg min-w-2xl object-fill"
+                        className=" menuItemImage lg:w-full rounded-t-lg object-cover "
                         src={element?.Image}
                         alt={element?.Alt}
                     />
-                </a>
-                <div className="p-5">
-                    <a>
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                </div>
+                <div className=" p-5 lg:min-h-72 ">
+                    <div>
+                        <h5 className=" menuItemText text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {element?.Title} {element?.Price ? "- $" + element?.Price : ""}
                         </h5>
-                    </a>
+                    </div>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                         {element?.Description}
                     </p>
                     <a
-                        className=" cursor-pointer prevent-select inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-yellow-700 dark:hover:bg-yellow-600 dark:focus:ring-blue-800"
+                        className="  cursor-pointer prevent-select inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-yellow-700 dark:hover:bg-yellow-600 dark:focus:ring-blue-800"
                         onClick={() => {
                             dispatch(
                                 addToCart({
